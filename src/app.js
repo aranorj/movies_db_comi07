9adac333126a44b006fd45f939b039521cf12d9f
aranorj <arano.rocio.b@gmail.com>
2024-04-25 08:00:53 -0300

    feat: url encoded

--- src/app.js
+++ src/app.js
@@ -12,9 +12,13 @@ const app = express();
 app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/', indexRouter);
 app.use(moviesRoutes);
