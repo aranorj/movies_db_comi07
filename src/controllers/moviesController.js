f7316ad00409f439beea2fe59f4271431c1f444f
aranorj <arano.rocio.b@gmail.com>
2024-04-25 21:45:04 -0300

    feat: editar una pelicula

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,13 +1,37 @@
-let db = require('../model/db/models')
+const peliculaService = require('../model/services/peliculaService');
 
 module.exports = {
     list: function(req, res) { 
-        db.Peliculas.findAll()
+        peliculaService.getAll()
         .then((peliculas)=>{
             res.render('moviesList', {movies: peliculas})
         })
-        .catch((error)=>{ 
-            console.log(error);
-            res.send("Ha opcurrido un erro al recuperar los datos, por favor reintente mas tarde")})
+        .catch((error)=>{
+            res.render('moviesList', {movies: error})
+        })       
+    },
+    edit: async function(req, res) {
+        try {
+            let pelicula = await peliculaService.getBy(req.params.id); 
+            res.render('editMovie', {pelicula: pelicula});  
+        } catch (error) {
+            res.send("Ha ocurrido un error inesperado").status(500);
+        }     
+    },
+    getOne: async function(req, res) {
+        try {
+            let pelicula = await peliculaService.getBy(req.params.id); 
+            res.render('moviesDetail', {movie: pelicula});  
+        } catch (error) {
+            res.send("Ha ocurrido un error inesperado").status(500);
+        }     
+    },
+    update: async function(req, res) {
+        try {
+            await peliculaService.updateBy(req.body, req.params.id);
+            res.redirect(`/movies/${req.params.id}`);
+        } catch (error) {
+            res.send("No se pudo editar!!");
+        }
     }
 }
---
+++ src/model/services/peliculaService.js
@@ -0,0 +1,52 @@
+let db = require('../db/models');
+
+module.exports = {
+    getAll: function() {
+        return new Promise((resolve, reject) => {
+            db.Peliculas.findAll()
+            .then(peliculas => {
+                //hacer cosas con mis peliculas 
+                resolve(peliculas)
+            })
+            .catch(err =>{
+                console.log(err);
+                reject([])
+            });
+        })         
+    },
+    getBy: async function(id) {
+        try {
+            return await db.Peliculas.findByPk(id);
+        } catch (error) {
+            console.log(error);
+            return {
+                id: 0,
+                title: "No encontrado",
+                rating: 0,
+                awards: 0,
+                length: 0,
+                release_date: ""
+            }
+        } 
+    },
+    updateBy: async function (body, id){
+        try {
+            let pelicula = new Pelicula(body);
+            await db.Peliculas.update(pelicula, {where: {id: id}});
+        } catch (error) {
+            //mala paractia de manejo de error agarrar y soltar uno nuevo 
+            //log y throw
+            //Si voy a hacer esto es preferible que no ponga el try catch y deje que el error suba solo 
+            console.log(error);
+            throw new Error("Un error");
+        }
+    }
+}
+
+function Pelicula({title, rating, awards, release_date, length}){
+    this.title = title;
+    this.rating = rating;
+    this.awards = awards;
+    this.release_date = release_date;
+    this.length = length;
+}
